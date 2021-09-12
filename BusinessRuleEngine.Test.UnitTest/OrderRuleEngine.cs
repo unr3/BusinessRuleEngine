f14ecbee2548aa762f57e686692fc1ef19360bca
gvs <gvs>
2021-09-12 18:21:06 +0200

    OrderRuleEngine tests added and passed

---
+++ BusinessRuleEngine.Test.UnitTest/OrderRuleEngine.cs
@@ -0,0 +1,31 @@
+﻿using BusinessRuleEngine.Abstraction;
+using BusinessRuleEngine.OrderRuleDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessRuleEngine.Test.UnitTest
+{
+    public class OrderRuleEngine : IRuleEngine<Order>
+    {
+        public IEnumerable<BusinessRule<Order>> Rules => _rules;
+        private BusinessRule<Order>[] _rules;
+        private readonly IRuleGenerator<Order> _ruleGenerator;
+        public OrderRuleEngine(IRuleGenerator<Order> ruleGenerator)
+        {
+            _ruleGenerator = ruleGenerator;
+        }
+
+        public void Apply(Order ruleObject)
+        {
+            _rules = _ruleGenerator.Generate();
+            if (_rules == null || !_rules.Any())
+                throw new ArgumentNullException("Rule list can not be empty");
+
+            for (int i = 0; i < _rules.Length; i++)
+            {
+                _rules[i].Execute(ruleObject);
+            }
+        }
+    }
+}
---
+++ BusinessRuleEngine.Test.UnitTest/OrderRuleEngineUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using BusinessRuleEngine.Abstraction;
+using BusinessRuleEngine.OrderRuleDomain.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine.Test.UnitTest
+{
+    public class OrderRuleEngineUnitTest
+    {
+        IRuleEngine<Order> _sut;
+        Mock<IRuleGenerator<Order>> _mockRuleGenerator;
+            
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockRuleGenerator= new Mock<IRuleGenerator<Order>>();
+            
+        }
+        [Test]
+        public void Should_ThrowError_When_CalledWithEmptyRuleList()
+        {
+            var order = new Order();
+            BusinessRule<Order>[] rules= null;
+            _mockRuleGenerator.Setup(s => s.Generate()).Returns(rules);
+            _sut = new OrderRuleEngine(_mockRuleGenerator.Object);
+
+            Assert.That(()=>_sut.Apply(order),Throws.ArgumentNullException);
+
+        }
+
+        [Test]
+        public void Should_NotThrowError_When_CalledWithRuleList()
+        {
+            var order = new Order();
+            var mockBusinessRule = new Mock<BusinessRule<Order>>();
+            BusinessRule<Order>[] rules = new BusinessRule<Order>[] {mockBusinessRule.Object };
+            _mockRuleGenerator.Setup(s => s.Generate()).Returns(rules);
+            _sut = new OrderRuleEngine(_mockRuleGenerator.Object);
+
+            Assert.DoesNotThrow(() => _sut.Apply(order));
+
+        }
+    }
+}
