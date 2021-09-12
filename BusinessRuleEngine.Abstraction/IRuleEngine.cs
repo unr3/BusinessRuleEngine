29b7b7236307ed1d03cdb828477ff7e6e427ab8d
gvs <gvs>
2021-09-12 17:39:54 +0200

    IRuleEngine interface added for base rune engine

---
+++ BusinessRuleEngine.Abstraction/IRuleEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine.Abstraction
+{
+   public interface IRuleEngine<T> where T:class
+    {
+        IEnumerable<BusinessRule<T>> Rules { get; }
+        void Apply();
+    }
+}
