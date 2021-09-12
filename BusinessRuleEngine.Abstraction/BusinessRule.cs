1eb7f9c1e8f8357a32b7f982baaf2bfcd6c100ac
gvs <gvs>
2021-09-12 13:48:45 +0200

    Improvement for BusinessRules
    Abstract BusinessRule class added  which will be the base class for all business rule classes

---
+++ BusinessRuleEngine.Abstraction/BusinessRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BusinessRuleEngine.Abstraction
+{
+    public abstract class BusinessRule<T> where T:class
+    {
+        public string RuleId => this.GetType().Name;
+        public bool IsSuccessful => _isSuccessful;
+        private bool _isSuccessful = false;
+        public abstract Func<T, bool> Condition { get; }
+        public abstract Predicate<T> Predicate { get; }
+
+        public virtual void Execute(T ruleObject)
+        {
+            if (Condition(ruleObject))
+            {
+                _isSuccessful = Predicate.Invoke(ruleObject);
+            }
+        }
+    }
+}
--- BusinessRuleEngine.OrderBusinessRules/Rules/GeneratePackingSlipRule.cs
+++ BusinessRuleEngine.OrderBusinessRules/Rules/GeneratePackingSlipRule.cs
@@ -1,28 +1,28 @@
-﻿using BusinessRuleEngine.OrderRuleDomain.Enums;
+﻿using BusinessRuleEngine.Abstraction;
+using BusinessRuleEngine.OrderRuleDomain.Enums;
 using BusinessRuleEngine.OrderRuleDomain.Models;
 using BusinessRuleEngine.OrderRuleDomain.Services;
-
+using System;
 
 namespace BusinessRuleEngine.OrderBusinessRules.Rules
 {
-    public class GeneratePackingSlipRule
+    public class GeneratePackingSlipRule :BusinessRule<Order>
     {
         private IPackingSlipService _packingSlipService;
 
-        public bool IsSuccessful => _isSuccessful;
-        private bool _isSuccessful = false;
+        
+       
+        public override Func<Order, bool> Condition => (Order o)=>o.Product.ProductType==ProductType.Physical;
+
+        public override Predicate<Order> Predicate => (Order o)=>_packingSlipService.GeneratePackingSlip(o);
+
+        
 
         public GeneratePackingSlipRule(IPackingSlipService packingSlipService)
         {
             _packingSlipService = packingSlipService;
         }
 
-        public void Execute(Order order)
-        {
-            if (order.Product.ProductType == ProductType.Physical)
-            {
-               _isSuccessful= _packingSlipService.GeneratePackingSlip(order);
-            }
-        }
+        
     }
 }
